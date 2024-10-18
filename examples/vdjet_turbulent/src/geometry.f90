51641fad15f8ef85c4b3bfcc503712df21f45226
Tereasa Spaulding <tereasaspaulding@gmail.com>
2024-10-18 17:29:26 -0400

    Add initial files

---
+++ examples/vdjet_turbulent/src/geometry.f90
@@ -0,0 +1,106 @@
+!> Various definitions and tools for initializing NGA2 config
+module geometry
+   use config_class, only: config
+   use precision,    only: WP
+   implicit none
+   private
+   
+   !> Single config
+   type(config), public :: cfg
+   
+   public :: geometry_init
+   
+contains
+   
+   
+   !> Initialization of problem geometry
+   subroutine geometry_init
+      use sgrid_class, only: sgrid
+      use param,       only: param_read
+      implicit none
+      type(sgrid) :: grid
+      
+      
+      ! Create a grid from input params
+      create_grid: block
+         use sgrid_class, only: cartesian
+         integer :: i,j,k,nx,ny,nz
+         real(WP) :: Lx,Ly,Lz
+         real(WP), dimension(:), allocatable :: x,y,z
+         integer :: j_max, j_min !> Added 10/14
+         real(WP) :: Djet, dy, nd
+
+         ! Read in grid definition
+         call param_read('Lx',Lx); call param_read('nx',nx); allocate(x(nx+1))
+         call param_read('Ly',Ly); call param_read('ny',ny); allocate(y(ny+1))
+         call param_read('Lz',Lz); call param_read('nz',nz); allocate(z(nz+1))
+         call param_read('D jet', Djet) ! Read in particle diameter
+
+         ! Create simple rectilinear grid
+         do i=1,nx+1
+            x(i)=real(i-1,WP)/real(nx,WP)*Lx
+         end do
+         
+         ! Original grid in y with no local refinement (10/14)
+         !do j=1,ny+1
+         !   y(j)=real(j-1,WP)/real(ny,WP)*Ly-0.5_WP*Ly
+         !end do
+         do k=1,nz+1
+            z(k)=real(k-1,WP)/real(nz,WP)*Lz-0.5_WP*Lz
+         end do
+
+         !> Added 10/14
+         ! Split y grid into 3 regions -> 2 with larger cells, 1 with smaller cells around jet inflow location
+         nd = 20; ! 20 points across y for locally refined mesh
+         dy = Ly/(ny-nd);
+         
+         ! Add buffer area of 1 grid cell to capture effects next to jet?
+         j_min = floor(-0.5_WP*Djet/dy + 0.5_WP*Ly/dy)  
+         j_max = j_min + 1 + nd
+
+         ! Region 1: lower half y domain
+         do j=1,j_min
+            y(j)=real(j-1,WP)/real(ny,WP)*Ly-0.5_WP*Ly
+         end do
+         ! Region 2: Area in y domain around jet
+         do j=j_min+1,j_max -1
+            y(j)= 1_WP/real(20,WP)*Djet + y(j-1) !real(j_min-1,WP)/real(ny,WP)*Ly-0.5_WP*Ly
+            print*, y(j)
+         end do
+         ! Region 3: upper half y domain
+         do j=j_max,ny+1
+            y(j)= 1_WP/real(ny,WP)*Ly + y(j-1) !real(j_max-2,WP)/real(ny,WP)*Ly-0.5_WP*Ly
+         end do
+ 
+         
+         ! General serial grid object
+         !grid=sgrid(coord=cartesian,no=2,x=x,y=y,z=z,xper=.false.,yper=.false.,zper=.false.,name='vdjet')
+         grid=sgrid(coord=cartesian,no=2,x=x,y=y,z=z,xper=.false.,yper=.true.,zper=.true.,name='vdjet')
+         
+      end block create_grid
+      
+      
+      ! Create a config from that grid on our entire group
+      create_cfg: block
+         use parallel, only: group
+         integer, dimension(3) :: partition
+         
+         ! Read in partition
+         call param_read('Partition',partition,short='p')
+         
+         ! Create partitioned grid
+         cfg=config(grp=group,decomp=partition,grid=grid)
+         
+      end block create_cfg
+      
+      
+      ! Create masks for this config
+      create_walls: block
+         cfg%VF=1.0_WP
+      end block create_walls
+      
+      
+   end subroutine geometry_init
+   
+   
+end module geometry
